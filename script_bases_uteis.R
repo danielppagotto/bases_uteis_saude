4a77dd3888a05238ff129e26a102a84c2e2bc873
danielppagotto <danielppagotto@gmail.com>
2021-08-31 10:48:22 -0300

    Create script_bases_uteis.R

---
+++ script_bases_uteis.R
@@ -0,0 +1,12 @@
+
+estabelecimentos <- read_csv("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/nome_estabelecimentos.csv",
+                             locale = locale(encoding = "latin1")) 
+
+cid10 <- read_csv2("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/cid_10.csv") %>% 
+  janitor::clean_names() 
+
+municipios <- read_csv2("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/lista_dados_sage.csv") 
+municipios$ibge <- as.character(municipios$ibge)
+
+estabelecimentos <- read_csv("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/nome_estabelecimentos.csv")
+
