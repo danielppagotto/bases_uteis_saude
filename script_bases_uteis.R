170242429b9e226c36a3d6202d1e4dacdbb0cc66
danielppagotto <danielppagotto@gmail.com>
2021-09-07 15:25:06 -0300

    Update script_bases_uteis.R

--- script_bases_uteis.R
+++ script_bases_uteis.R
@@ -10,3 +10,7 @@ municipios$ibge <- as.character(municipios$ibge)
 
 estabelecimentos <- read_csv("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/nome_estabelecimentos.csv")
 
+populacao <- read_delim("https://raw.githubusercontent.com/danielppagotto/bases_uteis_saude/main/populacao_municipios.csv",
+                        delim = ";") %>% 
+                      gather(key = "ano", value = "populacao", `2019`,`2020`,
+                             `2021`)
